35c7166a81074300adeda9d127e6fdfcf366d09b
Username <irene_gentili@PC-DI-IRENE.>
2024-11-17 11:18:15 +0100

    ADD

--- E02/esercizio2.py
+++ E02/esercizio2.py
@@ -15,9 +15,9 @@ plt.xscale('log')
 plt.yscale('log')
 plt.show()
 
-aly=pianeti['pl_orbsmax']/pianeti['st_mass']
+RqM=pianeti['pl_orbsmax']**2/pianeti['st_mass']
 
-plt.scatter(pianeti['pl_orbper'] , aly, color='tomato')
+plt.scatter(pianeti['pl_orbper'] , RqM, color='tomato')
 plt.xlabel('periodo', fontsize=16)
 plt.ylabel('Rmax/m*', fontsize=16)
 plt.xscale('log')
@@ -36,12 +36,48 @@ plt.xscale('log')
 plt.yscale('log')
 plt.show()
 
-trm=np.log(transit['pl_bmassj'])
-rvm=np.log(rad_vel['pl_bmassj'])
-n, bis, p = plt.hist(rvm, bins=200, range=(0, 8), color='gold', label='radial velocity',alpha=0.5 )
-n, bis, p = plt.hist(trm, bins=200, range=(0, 8), color='green',label='transit',alpha=0.5 )
+trm=np.log10(transit['pl_bmassj'])
+rvm=np.log10(rad_vel['pl_bmassj'])
+n, bis, p = plt.hist(rvm, bins=200, range=(-4, 4), color='gold', label='radial velocity',alpha=0.5 )
+n, bis, p = plt.hist(trm, bins=200, range=(-4, 4), color='green',label='transit',alpha=0.5 )
 plt.ylabel('massa', fontsize=16)
 plt.legend(fontsize=14)
 plt.show()
 
 
+
+fig = plt.figure(figsize=(12,11))
+
+
+gr = fig.add_gridspec(2, 2, hspace=0, wspace=0)
+ax = gr.subplots( sharex='col', sharey='row')
+
+ax[1,0].scatter(np.log10(transit['pl_orbper']),  trm,  color='limegreen',  alpha=0.3, label='Transit')
+ax[1,0].scatter( np.log10(rad_vel['pl_orbper']), rvm, color='darkorange', alpha=0.3, label='Radial Velocity')
+
+ax[1,0].tick_params(axis='both', which='major', labelsize=14)
+ax[1,0].set_xlabel('log(Period [days])',         fontsize=16)
+ax[1,0].set_ylabel('log(Planet Mass)',  fontsize=16)
+ax[1,0].legend(fontsize=16)
+
+
+ax[0,0].hist( np.log10(transit['pl_orbper']),bins=50, range=(-2, 5), color='limegreen',  alpha=0.5, label='Transit')
+
+ax[0,0].hist(np.log10(rad_vel['pl_orbper']), bins=50, range=(-2, 5), color='darkorange',  alpha=0.5, label='Radial Velocity')
+ax[0,0].tick_params(axis='both', which='major', labelsize=14)
+ax[0,0].set_ylabel('Numero pianeti',        fontsize=16)
+ax[0,0].legend(fontsize=16)
+
+
+
+ax[1,1].hist(trm,bins=50, range=(-4, 4), color='limegreen',  alpha=0.5, orientation='horizontal', label='Transit')
+
+ax[1,1].hist(rvm, bins=50, range=(-4, 4), color='darkorange',  alpha=0.5, orientation='horizontal', label='Radial Velocity')
+ax[1,1].tick_params(axis='both', which='major', labelsize=14)
+ax[1,1].set_xlabel( 'Number of planets',        fontsize=16)
+ax[1,1].legend(fontsize=16)
+
+ax[0,1].axis('off')
+
+plt.savefig('/home/irene_gentili/MCF/metodi-computazionali-per-la-fisica/E02/graficoriassuntivo.pdf')
+plt.show()
